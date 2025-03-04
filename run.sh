ef6c4487dab27b873d5b343460ba49e2970508d0
Domingo Dirutigliano <me@domy.sh>
2025-03-04 17:29:34 +0100

    test

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 docker build -t tex-builder ./utils || exit 1
-docker run -it --volume ./:/workspace/ tex-builder ./utils/build.sh || exit 1 
+docker run --volume ./:/workspace/ tex-builder ./utils/build.sh || exit 1 
 if [ "$1" == "open" ]; then
     open ./build/main.pdf
 fi
