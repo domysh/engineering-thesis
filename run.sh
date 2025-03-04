9e8e27b5084d0c9316895615832f8ddc747936a3
Domingo Dirutigliano <me@domy.sh>
2025-03-04 17:16:09 +0100

    test

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-docker run -it --volume ./:/workspace/ $(docker build -q ./utils) ./utils/build.sh
+docker run --volume ./:/workspace/ $(docker build -q ./utils) ./utils/build.sh
 if [ "$1" == "open" ]; then
     open ./build/main.pdf
 fi
