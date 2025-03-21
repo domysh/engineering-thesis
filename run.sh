9ceaa40651b38912130cb372beb495c4cba8a2ce
Domingo Dirutigliano <me@domy.sh>
2025-03-21 21:09:36 +0100

    per interpeter owned GIL explained

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 cd "$(dirname "$0")"
 
-docker build -t tex-builder ./utils || exit 1
-docker run --volume ./:/workspace/ tex-builder ./utils/build.sh || exit 1 
+(docker image ls | grep tex-builder &> /dev/null) || docker build -t tex-builder ./utils || exit 1
+docker run --volume ./:/workspace/ tex-builder ./utils/build.sh || exit 1
+
 if [ "$1" == "open" ]; then
     open ./build/main.pdf
 fi
