e01e2be87f9760786f44047648ef07b5f2f1a83d
Domingo Dirutigliano <me@domy.sh>
2025-03-25 18:44:51 +0100

    chapter 2 final fix

--- utils/build.sh
+++ utils/build.sh
@@ -2,7 +2,7 @@
 
 set -xe 
 mkdir -p build
-TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex 
-bibtex build/main
-TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex 
-TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex 
+(ls build/main.aux &> /dev/null) || TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex 
+bibtex build/main &&\
+TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex && \
+TEXINPUTS=./vendor//: pdflatex -interaction=nonstopmode -halt-on-error -file-line-error -shell-escape -output-directory=build main.tex
